145997ab199dd56311ab0da049bb80b846164e1d
Ellen Leahy <ellen@Ellens-MBP.connect>
2018-02-27 14:06:18 +0000

    Created a class for go button and a function in MAESTRO which creates fits file when clicked

--- MAESTRO.py
+++ MAESTRO.py
@@ -3,16 +3,20 @@ import tkinter as tk   # python3
 from fieldOfView import FieldOfView
 from lightCurve import LightCurve
 from Title import Title
+from goButton import GoButton
 
 class MAESTRO(tk.Frame):
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, width=1080, height=700)
 
-        title = Title(self, master)
-        fofv = FieldOfView(self, master)
-        lCurve = LightCurve(self, master)
-        fofv.createFits()
+        Title(self, master)
+        self._fofv = FieldOfView(self, master)
+        LightCurve(self, master)
+        GoButton(self, master)
+
+    def runMAESTRO(self, clicked):
+        self._fofv.createFits()
 
 if __name__ == "__main__":
     app = MAESTRO()
