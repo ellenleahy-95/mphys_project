3b26334b9ab97dc9db07c67bf76ce347887525b6
JoeStickley <joe@styx.co.uk>
2018-05-10 11:31:36 +0100

    commented timeInput and Title

--- Title.py
+++ Title.py
@@ -3,6 +3,7 @@ TITLE_FONT = ("Helvetica", 30, "bold")
 
 class Title(object):
     def __init__(self, app, master):
+        """Writes the title centred at the top of the gui, and authors names in the bottom right"""
         self._app = app
         self.tempTitle = tk.Label(master, text="WELCOME TO MAESTRO", font=TITLE_FONT)
         self.tempTitle.place(relx=0.5, rely=0, anchor = tk.N)
